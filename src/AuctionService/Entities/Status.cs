38159c3bbbf93f3715c18d801e8ed3872db16d5a
Tarun <tarunkrai@gmail.com>
2024-07-11 11:40:58 +0530

    End-of-Section-2

---
+++ src/AuctionService/Entities/Status.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuctionService.Entities
+{
+   
+    public enum Status
+    {
+        Live,
+        Finished,
+        ReserveNotMet
+    }
+    
+}
